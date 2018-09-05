6295608f8ad2ffd8b069aa0e0624f6ba255b615a
Ben Thompson <t.ben.thompson@gmail.com>
2018-09-05 14:13:06 -0400

    fixed setup.py --> faultea, set version

--- setup.py
+++ setup.py
@@ -9,17 +9,17 @@ except (IOError, ImportError):
     description = open('README.md').read()
 
 setup(
-    packages = ['cluda'],
+    packages = ['faultea'],
 
     install_requires = ['numpy'],
     zip_safe = False,
 
-    name = 'cluda',
+    name = 'faultea',
     version = version,
     description = '',
     long_description = description,
 
-    url = 'https://github.com/tbenthompson/cluda',
+    url = 'https://github.com/tbenthompson/faultea',
     author = 'T. Ben Thompson',
     author_email = 't.ben.thompson@gmail.com',
     license = 'MIT',
